54ade231b4b36f521918a7635217e217f22b76f7
JeffRReeder <43863681+JeffRReeder@users.noreply.github.com>
2020-10-02 11:58:14 -0700

    Memory game working Part 1

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const gameContainer = document.getElementById("game");
+let card1 = null;
+let card2 = null;
+let cardCount = 0;
+let stopClicking = false;
+
+
 
 const COLORS = [
   "red",
@@ -62,29 +68,85 @@ let numClicked = 0;
 function handleCardClick(event) {
   // you can use event.target to see which element was clicked
   console.log("you just clicked", event.target.className);
-  console.log("your class list is: ", event.target.classList);
+  let currentCard = event.target;
+
+  // card1 + card2 already selected, don't let user click anything more. Ref line 102
+  if(stopClicking === true){
+    return;
+  }
+  // if you have already flipped something, don't do anything (until they click something else) 
+  if(currentCard.classList.contains("flipped")){
+    console.log('you have clicked already!!!!!!!');
+    return;
+  }
+
   
+  
+  // 1. on click chnage bg color
+  currentCard.style.backgroundColor = currentCard.classList[0];
 
-    // to keep from clicking more than 2 cards at a time
-    if(numClicked < 2 ){
-      // clicking on card should change background color to be color of it's class.
-      event.target.style.backgroundColor = event.target.className;
-
-      // check 1st className vs 2nd className (aka a match)
-
-      numClicked++;
-      console.log('If num clicked is: ' + numClicked);
-      console.log(event.target.className);
-    
-      
-    } else{
-      numClicked = 0;
-      console.log(' ELSE numclick set to zero?: ' + numClicked);
+  // 1st run no cards assigned
+  if(!card1 || !card2){
+    // add class "flipped" to whoever
+    currentCard.classList.add("flipped");
+    // set card1 to currentCard or itself
+    card1 = card1 || currentCard;
+    if (currentCard === card1){
+      card2 = null;
+    } else {
+      card2 = currentCard;
     }
-    
-    console.log(' AFTER if num click total is: ' + numClicked);
-  
-  
+  }
+
+  // prevent user from clicking more than 2 cards
+  // compare cards, if same remove event listener
+  if(card1 && card2){
+    stopClicking = true;
+    // cards are a match
+    if (card1.classList[0] === card2.classList[0]){
+      cardCount += 2;
+      console.log("card cound is: ",cardCount);
+      // stop looking for clicks on these cards
+      card1.removeEventListener("click", handleCardClick);
+      card2.removeEventListener("click", handleCardClick);
+      // reset values for next round or choices
+      card1 = null;
+      card2 = null;
+      stopClicking = false;
+      
+    } else {
+      // not a match
+      setTimeout(function(){
+        // removed class flipped
+        card1.classList.remove("flipped");
+        card2.classList.remove("flipped");
+        // set background color to default
+        card1.style.backgroundColor = '';
+        card2.style.backgroundColor = '';
+        // reset values for next round or choices
+        card1 = null;
+        card2 = null;
+        stopClicking = false;
+      },1000);  
+    }
+    // game is complete, so reset game
+    if(cardCount === COLORS.length){
+      alert("all done");
+      //const button = document.createElement('button[type="submit"]');
+      
+    }
+  }
+
+
+  console.log(cardCount, card1, card2);
+
+  // 2. add class flipped
+  //event.target.classList.add('flipped');
+ 
+  // 2nd click change bg color, add class of 'flopped' , save card 2
+  // compare cards
+  // if match, change class from flipped to done
+  // if not match, call setTimeout and remove class of 'flipped', and clear card1/card2
 
   // Users should only be able to change at most 2 cards at a time.
   // Clicking on 2 matching cars is a "Match" - those cards stay face up.
